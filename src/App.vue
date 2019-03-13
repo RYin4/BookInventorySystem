00a78d8b77fdca407d5a74ddf91d5185aa507c54
Randy Yin <randyyin28@gmail.com>
2019-03-12 18:05:50 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import Firebase from 'firebase'
+
+//access to the firebase api 
+let config = {
+    apiKey: "AIzaSyDyG2Ximhha4c0g1D_yVX0pPegbxuxGctM",
+    authDomain: "vuejs-firebase-dac4d.firebaseapp.com",
+    databaseURL: "https://vuejs-firebase-dac4d.firebaseio.com",
+    projectId: "vuejs-firebase-dac4d",
+    storageBucket: "vuejs-firebase-dac4d.appspot.com",
+    messagingSenderId: "755924921672"
+}
+
+//establish connection to firebase
+let app = Firebase.initializeApp(config);
+let db = app.database();
+
+//ref to books node in the firebase db
+let bookRef = db.ref('books');
+
+
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  //data access
+  firebase: {
+    books: booksRef
   }
 }
 </script>
